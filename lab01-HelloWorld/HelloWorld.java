edf33a84fb0585db8402a1b6e4f4dea7cfcb1b22
arikbaer <anb218@lehigh.edu>
2014-08-26 21:37:13 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,28 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//lab01-HelloWorld
+//Hello World Java Program
+//
+//first compile the program
+//  javac HelloWorld.java
+//run the program
+//java HelloWorld//
+
+// here we define a class
+public class HelloWorld{
+    
+//next, we add a method
+public static void main(String[] args) {
+    
+// we now print the statement with the box around it
+    System.out.println("########################");
+    System.out.println("########################");
+    System.out.println("#                      #");
+    System.out.println("#  Hello World 2014!!  #");
+    System.out.println("#                      #");
+    System.out.println("########################");
+    System.out.println("########################");
+    System.out.println("test");
+    
+    //test/testtest
+}
+}
