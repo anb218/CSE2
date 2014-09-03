30934486d87559046ccaf9ce6e02d05204889cc8
arikbaer <anb218@lehigh.edu>
2014-09-03 20:41:04 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,29 @@
+/////////////////////////////////////////////////////////////////////////////
+// hw01-WelcomeClass
+// Welcome Class Java Program
+//
+//Arik Baer
+// here we define a class
+public class WelcomeClass{
+    
+    //next, we add the main method
+    public static void main(String[] args) {
+        
+    // Now we print the Welcome text with the box around it
+    System.out.println("   -----------");
+    System.out.println("   | WELCOME |");
+    System.out.println("   -----------");
+    
+    // Next we print our lehigh username surrounded by its diamond shaped box
+    System.out.println("  ^  ^  ^  ^  ^  ^  ");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("|-A--N--B--2--1--8-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+    System.out.println("  v  v  v  v  v  v  ");
+    
+    // Then we print the tweet-long autobiographic statement
+    System.out.println("This June my uncle and I ran a full marathon in hot and humid Singapore.");
+    
+    }
+    
+}
